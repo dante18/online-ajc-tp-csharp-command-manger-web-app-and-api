184ecbdac1b83d6a940baf48ab6a3a2351a892d7
dante18 <lablandinerie2@gmail.com>
2025-09-30 10:18:44 +0200

    Init solution with initial structure

---
+++ EatDomicile.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EatDomicile.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
