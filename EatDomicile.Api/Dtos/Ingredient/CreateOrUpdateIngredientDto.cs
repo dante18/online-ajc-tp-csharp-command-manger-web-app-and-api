a2528e0b6f272a51c84b2565e3fb6e8c22e05435
Eric Ghoubiguian <ericghoubiguian@live.fr>
2025-09-30 17:19:42 +0200

    Ingredients in the API routes

---
+++ EatDomicile.Api/Dtos/Ingredient/CreateOrUpdateIngredientDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EatDomicile.Api.Dtos.Ingredient
+{
+    public class CreateOrUpdateIngredientDto
+    {
+        [Required]
+        public decimal KCal { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public bool IsAllergen { get; set; }
+    }
+}
---
+++ EatDomicile.Api/Dtos/Ingredient/IngredientDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EatDomicile.Api.Dtos.Ingredient
+{
+    public class IngredientDto
+    {
+        public decimal KCal { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsAllergen { get; set; }
+    }
+}
