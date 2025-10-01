dbed49d01776fc465a5ea4da34e32f329b7f0b81
hgxv <kevguillot99@gmail.com>
2025-10-01 11:49:26 +0200

    PizzaService init

---
+++ EatDomicile.Web.Services/Pizzas/DTO/PizzaDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EatDomicile.Web.Services.Pizzas.DTO;
+
+public class PizzaDTO
+{
+    public int? Id { get; set; }
+
+    public string Name { get; set; }
+
+    public decimal Price { get; set; }
+
+    public DoughsDto Doughs { get; set; }
+
+    public bool Vegetarian { get; set; }
+}
