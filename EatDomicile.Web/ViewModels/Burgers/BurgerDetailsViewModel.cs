0180573198af35ae77f712cc9addfb1cb34b6988
dante18 <lablandinerie2@gmail.com>
2025-10-01 14:54:47 +0200

    Feat: Add class used to display viesw used to manage burgers (view, vieswsModel)

---
+++ EatDomicile.Web/ViewModels/Burgers/BurgerDetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using EatDomicile.Web.Services.Burgers.DTO;
+using EatDomicile.Web.Services.Ingredient.DTO;
+
+namespace EatDomicile.Web.ViewModels.Burgers;
+
+public class BurgerDetailsViewModel
+{
+    public BurgerDTO Burger { get; set; }
+
+    public IEnumerable<IngredientDTO> Ingredients { get; set; }
+}
