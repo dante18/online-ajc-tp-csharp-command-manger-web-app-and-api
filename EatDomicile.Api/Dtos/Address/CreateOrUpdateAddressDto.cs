986d17cb3ae8b1548fced94edf0b762cc346bb8e
Eric Ghoubiguian <ericghoubiguian@live.fr>
2025-10-01 11:39:26 +0200

    Initialisation of some Dto and of UserController

---
+++ EatDomicile.Api/Dtos/Address/CreateOrUpdateAddressDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EatDomicile.Api.Dtos.Address
+{
+    public class CreateOrUpdateAddressDto
+    {
+        [Required]
+        public string Street { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Required]
+        public string Zip { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+    }
+}
