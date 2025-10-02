cf5b8314e8b9cbaca32423f52bbf4b6dd43cfb62
dante18 <lablandinerie2@gmail.com>
2025-10-02 15:28:02 +0200

    Feat: Add files used to link web to api

---
+++ EatDomicile.Web.Services/Orders/DTO/OrderProductDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EatDomicile.Web.Services.Orders.DTO;
+
+public class OrderProductDto
+{
+    public int Id { get; set; }
+
+    public ProductDto Product { get; set; }
+}
---
+++ EatDomicile.Web.Services/Orders/DTO/ProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace EatDomicile.Web.Services.Orders.DTO;
+
+public class ProductDto
+{
+    public int? Id { get; set; }
+
+    public string Name { get; set; }
+
+    public decimal Price { get; set; }
+}
