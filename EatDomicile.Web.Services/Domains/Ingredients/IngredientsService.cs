5b9a820ffc7b1149a95992c753c8b1eeadd58bbc
dante18 <lablandinerie2@gmail.com>
2025-10-03 18:17:12 +0200

    feat: Add and implements IApiService interface. And add error message block. And fix broken link to other app block

--- EatDomicile.Web.Services/Domains/Ingredients/IngredientsService.cs
+++ EatDomicile.Web.Services/Domains/Ingredients/IngredientsService.cs
@@ -1,8 +1,10 @@
-﻿using EatDomicile.Web.Services.Ingredient.DTO;
-using System.Net.Http.Json;
+﻿using System.Net.Http.Json;
+using EatDomicile.Web.Services.Domains.Ingredients.DTO;
+using EatDomicile.Web.Services.Interfaces;
 
-namespace EatDomicile.Web.Services.Ingredients;
-public class IngredientsService
+namespace EatDomicile.Web.Services.Domains.Ingredients;
+
+public class IngredientsService : IApiIngredientsService
 {
     private readonly HttpClient httpClient;
 
@@ -10,6 +12,7 @@ public class IngredientsService
     {
         this.httpClient = httpClient;
     }
+
     public async Task<IEnumerable<IngredientDTO>> GetIngredientsAsync()
     {
         var ingredients = await httpClient.GetFromJsonAsync<IEnumerable<IngredientDTO>>("https://localhost:7001/api/ingredients");
@@ -24,19 +27,19 @@ public class IngredientsService
 
     public async Task CreateIngredientAsync(IngredientDTO ingredientDTO)
     {
-        var response = await this.httpClient.PostAsJsonAsync("https://localhost:7001/api/ingredients", ingredientDTO);
+        var response = await httpClient.PostAsJsonAsync("https://localhost:7001/api/ingredients", ingredientDTO);
         _ = response.EnsureSuccessStatusCode();
     }
 
     public async Task UpdateIngredientAsync(int id, IngredientDTO ingredientDTO)
     {
-        var response = await this.httpClient.PutAsJsonAsync($"https://localhost:7001/api/ingredients/{id}", ingredientDTO);
+        var response = await httpClient.PutAsJsonAsync($"https://localhost:7001/api/ingredients/{id}", ingredientDTO);
         _ = response.EnsureSuccessStatusCode();
     }
 
     public async Task DeleteIngredientAsync(int id)
     {
-        var response = await this.httpClient.DeleteAsync($"https://localhost:7001/api/ingredients/{id}");
+        var response = await httpClient.DeleteAsync($"https://localhost:7001/api/ingredients/{id}");
         _ = response.EnsureSuccessStatusCode();
     }
 }
