587faa3aff0ac2a1432fa101581f99703ccf061b
dante18 <lablandinerie2@gmail.com>
2025-10-04 12:44:11 +0200

    archi: Add extension method used to load services use in web

---
+++ EatDomicile.Web.Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using EatDomicile.Web.Services.Domains.Burgers;
+using EatDomicile.Web.Services.Domains.Doughs;
+using EatDomicile.Web.Services.Domains.Drinks;
+using EatDomicile.Web.Services.Domains.Ingredients;
+using EatDomicile.Web.Services.Domains.Orders;
+using EatDomicile.Web.Services.Domains.Pastas;
+using EatDomicile.Web.Services.Domains.Pizzas;
+using EatDomicile.Web.Services.Domains.Statistics;
+using EatDomicile.Web.Services.Domains.Users;
+using EatDomicile.Web.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace EatDomicile.Web.Services.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddEatDomicileService(this IServiceCollection services, IConfiguration config)
+    {
+        services.TryAddTransient<IApiDrinksService, DrinksService>();
+        services.TryAddTransient<IApiDoughsService, DoughsService>();
+        services.TryAddTransient<IApiIngredientsService, IngredientsService>();
+        services.TryAddTransient<IApiBurgersService, BurgersService>();
+        services.TryAddTransient<IApiPizzasService, PizzasService>();
+        services.TryAddTransient<IApiPastasService, PastasService>();
+        services.TryAddTransient<IApiUsersService, UsersService>();
+        services.TryAddTransient<IApiOrdersService, OrdersService>();
+        services.TryAddTransient<IApiProductsService, ProductsService>();
+        services.TryAddTransient<IApiStatisticsService, StatisticsService>();
+
+        return services;
+    }
+}
